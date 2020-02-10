0a299417212e32af82888e06bc4ba8b2fc507bed
amanjagga1 <aman.jagga@practo.com>
2020-02-11 01:24:21 +0530

    add build

---
+++ docs/precache-manifest.1198937c0dcaa234a14fed05b9e3c880.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "968ac1e98e541f96627012573a8092aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "383f5c069d01364e0e56",
+    "url": "/static/css/main.86f0524b.chunk.css"
+  },
+  {
+    "revision": "2ba6b1daed03312ea7e6",
+    "url": "/static/js/2.c4abdfc2.chunk.js"
+  },
+  {
+    "revision": "967c3aa677d48917bda81b3b2ff2693c",
+    "url": "/static/js/2.c4abdfc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "383f5c069d01364e0e56",
+    "url": "/static/js/main.50daf417.chunk.js"
+  },
+  {
+    "revision": "9807e590b7c6e8a3aacf",
+    "url": "/static/js/runtime-main.675bd575.js"
+  }
+]);
